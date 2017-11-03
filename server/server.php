feed7d480072d1d58a23f71e2e53677c1c1da2b7
Gabi DJ <git@gabisuciu.ro>
2017-11-03 13:54:15 +0200

    moved resolved version

--- server/server.php
+++ server/server.php
@@ -7,13 +7,16 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use ChatApp\Chat;
 
+
+$port = 8080;
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
             new Chat()
         )
     ),
-    8080
+    $port
 );
 
 $server->run();
